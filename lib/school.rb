ac76d41fa0f69ac774201aeb16aed896cd6346b1
Thomas Edwards <thce28@gmail.com>
2019-02-21 11:58:11 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,40 @@
-# code here!
+require 'pry'
+class School
+
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+
+  def add_student(stud_name, age)
+
+
+
+      if roster.key?(age)
+        roster[age] << stud_name
+
+      else
+        roster[age] = []
+        roster[age] << stud_name
+      end
+    roster
+    end
+
+    def grade(grade)
+      roster[grade]
+    end
+
+    def sort
+
+      roster.each { |key, value| value.sort!}
+
+    end
+
+
+  end
